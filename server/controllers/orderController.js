54e3fe0249ecf8232bace79bfbfed23ae9f43a7f
Danish-mohiudin <danishmohiudin14@gmail.com>
2025-08-03 18:48:47 +0530

    backend compleated

---
+++ server/controllers/orderController.js
@@ -0,0 +1,59 @@
+// place order cod : /api/order/cod
+
+import Order from "../models/orderSchema.js";
+import Product from "../models/ProductSchema.js";
+
+export const placeOrderCOD = async (req,res)=>{
+    try {
+        const {userId, items, address} = req.body;
+        if(!address || items.length === 0){
+            return res.json({success:false, message:"invalid data"})
+        }
+        // calculate ammount using items
+        let amount = await items.reduce(async(acc,item)=>{
+            const product = await Product.findById(item.product);
+            return (await acc) + product.offerPrice * item.quantity;
+        },0)
+        // add tax charges (2%)
+        amount += Math.floor( amount * 0.02);
+
+        await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType:'COD'
+        });
+        return res.json({success:true, message:"Order Placed Successfully"})
+    } catch (error) {
+        return res.json({success:false, message:error.message})
+    }
+}
+
+// get orders by userId : api/order/user
+
+export const getUserOrders = async (req,res)=>{
+    try {
+        const {userId} = req.body;
+        const orders = await Order.find({
+            userId,
+            $or:[{paymentType:'COD'}, {isPaid: true}] //paymentType is 'COD' (Cash on Delivery), OR isPaid is true (user has already paid online).
+        }).populate("items.product address").sort({createdAt: -1}); // .populate => It just finds that ID and then fills the complete data from another schema to which it is referenced.
+        return res.json({success:true, orders})
+    } catch (error) {
+        return res.json({success:false, message:error.message});
+    }
+}
+
+// get All Orders ( seller / admin) : /api/order/seller  ==> it will return all orders for admin or seller
+
+export const getAllOrders = async (req,res)=>{
+    try {
+        const orders = await Order.find({
+            $or:[{paymentType:'COD'}, {isPaid: true}]
+        }).populate("items.product address").sort({createdAt: -1}) //sort => newly ordered items will be at the top, 
+        return res.json({success:true, orders})
+    } catch (error) {
+        return res.json({success:false, message:error.message});
+    }
+}
