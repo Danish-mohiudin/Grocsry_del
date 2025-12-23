e989c44e35cd094b0facf273f4039f73bd94cfe2
Danish-mohiudin <danishmohiudin14@gmail.com>
2025-12-23 22:04:34 +0530

    bug

--- server/models/orderSchema.js
+++ server/models/orderSchema.js
@@ -1,16 +1,41 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-    userId:{type:String, required:true, ref:'user'},
+    userId: { 
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user',
+        required:true 
+    },
     items:[{
-        product: {type:String, required:true, ref:'product'},
-        quantity: {type:Number, required:true},
+        product: { 
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'product',
+            required:true 
+        },
+        quantity: {
+            type:Number, 
+            required:true
+        },
     }],
-    ammount: {type:Number, required:true},
-    address: {type:String, required:true, ref:'address'},
-    status: {type:String, default:'order placed'},
-    paymentType: {type:String, required:true},
-    isPaid: {type:Boolean, required:true,default:false}
+    ammount: {
+        type:Number, 
+        required:true},
+    address: {
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'address',
+        required:true 
+    },
+    status: {
+        type:String, 
+        default:'order placed'},
+
+    paymentType: {
+        type:String, 
+        required:true},
+
+    isPaid: {
+        type:Boolean, required:true,default:false
+    }
 },{timestamps:true})
 
 const Order = mongoose.models.Order || mongoose.model('order', orderSchema);
